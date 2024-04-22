9ab79d9ecc03d9edd46a31943feb1c41311095f1
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-04-22 12:55:15 +0200

    Ändrat om, menas det att bilarna ska vara object så som trådar?

--- Models/Events.cs
+++ Models/Events.cs
@@ -9,11 +9,8 @@ namespace Labb2Trådar.Models
     internal class Events
     {
         static ThreadLocal<int> _current = new ThreadLocal<int>();
-        public int probability = 50;
 
 
-        public Thread car1 = new Thread(CompStart);
-        public Thread car2 = new Thread(CompStart);
         public static List<Car> GetCars()
         {
             return new List<Car>
@@ -25,39 +22,83 @@ namespace Labb2Trådar.Models
 
         public static void Competition()
         {
+            Thread car1 = new Thread(() => CompStart("Bil Ett", 120))
+            {
+                Name = "Bil Ett"
+
+            };
+            Thread car2 = new Thread(() => CompStart("Bil Två", 120))
+            {
+                Name = "Bil Två"
+            };
+            while (true)
+            {
+
+                car1.Start();
+                car2.Start();
+
+
+            }
 
         }
-        public static void CompStart()
+        public static void Randomiser()
         {
-            int SpeedPerHour = 120;
+            Random rand = new Random();
+            int random150 = rand.Next(1, 51); // Ger ett nummer mellan 1-50 (1/50 risk)
+            if (random150 == 19)
+            {
+                Gas();
+            }
+            else if (random150 == 17 || random150 == 18) // (6/50 risk)
+            {
+                PoppedTire();
+            } 
+            else if (random150 >= 11 || random150 <= 16) //(5/50 risk) 
+            {
+                BirdWindshield();
+            }
+            else if (random150 >= 1 || random150 <= 10) // (10/50 risk)
+            {
+                MotorProblem();
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+        public static void CompStart(string carName, int carSpeed)
+        {
             int totalLength = 10000;
-
-                for (int i = 0; i < totalLength; i++)
+            Console.WriteLine($"Car with name {Thread.CurrentThread.Name} started racing!");
+            for (int i = 0; i < totalLength;)
                 {
+                    i++;
+                    for (i = )
 
                 }
             
         }
         public static void Gas()
         {
-            GetCars();
-            //1/50 prob.
-            Console.WriteLine($"{car1}");
+
+            Console.WriteLine($"Gas refill event occurred for {Thread.CurrentThread.Name}!\n{Thread.CurrentThread.Name} stops for 30 sek.");
+            Thread.Sleep(3000);
 
         }
         public static void PoppedTire() 
         {
-            //2/50
+            Console.WriteLine($"Popped tired event occured!\n{Thread.CurrentThread.Name} stops for 20 sek.");
+            Thread.Sleep(2000);
         }
         public static void BirdWindshield()
         {
-            //5/50
+            Console.WriteLine($"Birds on the windshield event occured!\n{Thread.CurrentThread.Name} stops for 10 sek.");
+            Thread.Sleep(1000);
+
         }
         public static void MotorProblem()
         {
-            //10/50
-            //var Car = new Car();
-            //Car.SpeedPerHour = Car.SpeedPerHour - 1;
+            Console.WriteLine("Motor problem event occured!\n{car} Speed per hour decreses from {car.speed} to ");
             
         }
     }
